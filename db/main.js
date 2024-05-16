afb80bcf99bfe4a6345faf087cfd5d25a616d5ca
dromggo <162737995+dromggo@users.noreply.github.com>
2024-05-15 18:14:10 -0600

    Removed return

--- db/main.js
+++ db/main.js
@@ -4,7 +4,6 @@ function db() {
   let instance;
 
   async function createInstance() {
-    return true;
     console.log('Connecting to db...');
     let db = await mongoose.connect(process.env.MONGO_URL);
     console.log('Connected');
